7273acab720bbec8f2df8bd309f121e0e68581aa
Martin Polden <mpolden@mpolden.no>
2019-06-02 15:15:49 +0200

    Implement hosts package

---
+++ hosts/hosts.go
@@ -0,0 +1,105 @@
+package hosts
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+)
+
+// LocalNames represent host names that are considered local.
+var LocalNames = []string{
+	"localhost",
+	"localhost.localdomain",
+	"local",
+	"broadcasthost",
+	"ip6-localhost",
+	"ip6-loopback",
+	"ip6-localnet",
+	"ip6-mcastprefix",
+	"ip6-allnodes",
+	"ip6-allrouters",
+	"ip6-allhosts",
+	"0.0.0.0",
+}
+
+// DefaultParser is the default parser
+var DefaultParser = &Parser{IgnoredHosts: LocalNames}
+
+// Parser represents a hosts parser.
+type Parser struct {
+	IgnoredHosts []string
+}
+
+// Hosts represents a hosts file.
+type Hosts struct {
+	entries map[string][]net.IPAddr
+}
+
+// Parse uses DefaultParser to parse hosts from reader r.
+func Parse(r io.Reader) (*Hosts, error) {
+	return DefaultParser.Parse(r)
+}
+
+// Get returns the IP addresses of name.
+func (h *Hosts) Get(name string) ([]net.IPAddr, bool) {
+	ipAddrs, ok := h.entries[name]
+	return ipAddrs, ok
+}
+
+// Len returns the number of entries.
+func (h *Hosts) Len() int { return len(h.entries) }
+
+func (p *Parser) ignore(name string) bool {
+	for _, ignored := range p.IgnoredHosts {
+		if ignored == name {
+			return true
+		}
+	}
+	return false
+}
+
+// Parse parses HOSTS from reader r.
+func (p *Parser) Parse(r io.Reader) (*Hosts, error) {
+	entries := make(map[string][]net.IPAddr)
+	scanner := bufio.NewScanner(r)
+	n := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			continue
+		}
+		ipAddr, err := net.ResolveIPAddr("", fields[0])
+		if err != nil {
+			return nil, fmt.Errorf("line %d: invalid ip address: %s", n, fields[0])
+		}
+		for _, name := range fields[1:] {
+			if p.ignore(name) {
+				continue
+			}
+			entries[name] = append(entries[name], *ipAddr)
+		}
+		n++
+	}
+	return &Hosts{entries: entries}, nil
+}
+
+// Combine combines multiple Hosts into one. If name is present in multiple hosts, the first encountered name is kept
+// and duplicates are discarded.
+func Combine(hosts ...*Hosts) *Hosts {
+	entries := make(map[string][]net.IPAddr)
+	for _, h := range hosts {
+		for name, ipAddr := range h.entries {
+			if _, ok := entries[name]; ok {
+				continue // Already added
+			}
+			entries[name] = ipAddr
+		}
+	}
+	return &Hosts{entries: entries}
+}
---
+++ hosts/hosts_test.go
@@ -0,0 +1,127 @@
+package hosts
+
+import (
+	"net"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type test struct {
+	in  string
+	out []string
+	ok  bool
+}
+
+func testParser(p *Parser, hosts string, tests []test, t *testing.T) {
+	h, err := p.Parse(strings.NewReader(hosts))
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, tt := range tests {
+		ipAddrs, ok := h.Get(tt.in)
+		var got []string
+		for _, ipAddr := range ipAddrs {
+			got = append(got, ipAddr.String())
+		}
+		if ok != tt.ok || !reflect.DeepEqual(got, tt.out) {
+			t.Errorf("#%d: Get(%q) = (%v, %t), want (%v, %t)", i, tt.in, got, ok, tt.out, tt.ok)
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	in := `
+# comment
+
+incomplete-line
+
+127.0.0.1       localhost
+127.0.0.1       localhost.localdomain
+127.0.0.1       local
+255.255.255.255 broadcasthost
+::1             localhost
+::1             ip6-localhost
+::1             ip6-loopback
+fe80::1%lo0     localhost
+ff00::0         ip6-localnet
+ff00::0         ip6-mcastprefix
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+ff02::3         ip6-allhosts
+0.0.0.0         0.0.0.0
+192.0.2.1       test1 test2 test3
+192.0.2.2       test4
+192.0.2.3       test5
+192.0.2.1       test6
+`
+
+	var tests1 = []test{
+		{"test1", []string{"192.0.2.1"}, true},
+		{"test2", []string{"192.0.2.1"}, true},
+		{"test3", []string{"192.0.2.1"}, true},
+		{"test4", []string{"192.0.2.2"}, true},
+		{"test5", []string{"192.0.2.3"}, true},
+		{"nonexistent", nil, false},
+		{"localhost", nil, false},
+		{"localhost.localdomain", nil, false},
+		{"local", nil, false},
+		{"broadcasthost", nil, false},
+		{"ip6-localhost", nil, false},
+		{"ip6-loopback", nil, false},
+		{"ip6-localnet", nil, false},
+		{"ip6-mcastprefix", nil, false},
+		{"ip6-allnodes", nil, false},
+		{"ip6-allrouters", nil, false},
+		{"ip6-allhosts", nil, false},
+		{"0.0.0.0", nil, false},
+	}
+
+	testParser(DefaultParser, in, tests1, t)
+
+	var tests2 = []test{
+		{"test1", []string{"192.0.2.1"}, true},
+		{"test2", []string{"192.0.2.1"}, true},
+		{"test3", []string{"192.0.2.1"}, true},
+		{"test4", []string{"192.0.2.2"}, true},
+		{"test5", []string{"192.0.2.3"}, true},
+		{"nonexistent", nil, false},
+		{"localhost", []string{"127.0.0.1", "::1", "fe80::1%lo0"}, true},
+		{"localhost.localdomain", []string{"127.0.0.1"}, true},
+		{"local", []string{"127.0.0.1"}, true},
+		{"broadcasthost", []string{"255.255.255.255"}, true},
+		{"ip6-localhost", []string{"::1"}, true},
+		{"ip6-loopback", []string{"::1"}, true},
+		{"ip6-localnet", []string{"ff00::"}, true},
+		{"ip6-mcastprefix", []string{"ff00::"}, true},
+		{"ip6-allnodes", []string{"ff02::1"}, true},
+		{"ip6-allrouters", []string{"ff02::2"}, true},
+		{"ip6-allhosts", []string{"ff02::3"}, true},
+		{"0.0.0.0", []string{"0.0.0.0"}, true},
+	}
+	testParser(&Parser{}, in, tests2, t)
+}
+
+func TestCombine(t *testing.T) {
+	hosts1 := &Hosts{entries: map[string][]net.IPAddr{
+		"test1": {{IP: net.ParseIP("192.0.2.1")}},
+		"test2": {{IP: net.ParseIP("192.0.2.2")}},
+	}}
+	hosts2 := &Hosts{entries: map[string][]net.IPAddr{
+		"test1": {{IP: net.ParseIP("192.0.2.254")}}, // Ignored duplicate
+		"test3": {{IP: net.ParseIP("192.0.2.3")}},
+		"test4": {{IP: net.ParseIP("192.0.2.4")}},
+	}}
+
+	hosts := Combine(hosts1, hosts2)
+	want := map[string][]net.IPAddr{
+		"test1": {{IP: net.ParseIP("192.0.2.1")}},
+		"test2": {{IP: net.ParseIP("192.0.2.2")}},
+		"test3": {{IP: net.ParseIP("192.0.2.3")}},
+		"test4": {{IP: net.ParseIP("192.0.2.4")}},
+	}
+
+	if !reflect.DeepEqual(hosts.entries, want) {
+		t.Errorf("want %+v, got %+v", want, hosts.entries)
+	}
+}
