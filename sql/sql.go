7bfc855a2040dca14cacd05aeb833f0714292c53
Martin Polden <mpolden@mpolden.no>
2019-07-10 20:41:19 +0200

    Rewrite sql package

--- sql/sql.go
+++ sql/sql.go
@@ -9,27 +9,51 @@ import (
 )
 
 const schema = `
-CREATE TABLE IF NOT EXISTS question (
-  id            INTEGER         PRIMARY KEY,
-  value         TEXT            NOT NULL,
-  CONSTRAINT    value_unique    UNIQUE (value)
+CREATE TABLE IF NOT EXISTS rr_question (
+  id                INTEGER           PRIMARY KEY,
+  name              TEXT              NOT NULL,
+  CONSTRAINT        name_unique       UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS answer (
-  id            INTEGER         PRIMARY KEY,
-  value         TEXT            NOT NULL,
-  CONSTRAINT    value_unique    UNIQUE(value)
+CREATE TABLE IF NOT EXISTS rr_answer (
+  id                INTEGER           PRIMARY KEY,
+  name              TEXT              NOT NULL,
+  CONSTRAINT        name_unique       UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS rr_type (
+  id                INTEGER           PRIMARY KEY,
+  type              INTEGER           NOT NULL,
+  CONSTRAINT        type_unique       UNIQUE(type)
 );
 
 CREATE TABLE IF NOT EXISTS log (
-  id            INTEGER        PRIMARY KEY,
-  time          INTEGER        NOT NULL,
-  question_id   INTEGER        NOT NULL,
-  answer_id     INTEGER        NOT NULL,
-  FOREIGN       KEY(question_id) REFERENCES question(id),
-  FOREIGN       KEY(answer_id) REFERENCES answer(id)
+  id                INTEGER           PRIMARY KEY,
+  time              INTEGER           NOT NULL,
+  rr_type_id        INTEGER           NOT NULL,
+  rr_question_id    INTEGER           NOT NULL,
+  FOREIGN KEY       (rr_question_id)  REFERENCES rr_question(id),
+  FOREIGN KEY       (rr_type_id)      REFERENCES rr_type(id)
+);
+
+CREATE TABLE IF NOT EXISTS log_rr_answer (
+  id                INTEGER           PRIMARY KEY,
+  log_id            INTEGER           NOT NULL,
+  rr_answer_id      INTEGER           NOT NULL,
+  FOREIGN KEY       (log_id)          REFERENCES log(id),
+  FOREIGN KEY       (rr_answer_id)    REFERENCES rr_answer(id)
 );
 `
+
+/*
+INSERT INTO rr_question (name) VALUES ('foo.bar');
+INSERT INTO rr_type (type) VALUES (15);
+INSERT INTO rr_answer (name) VALUES ('1.1.1.1');
+INSERT INTO rr_answer (name) VALUES ('1.0.0.1');
+INSERT INTO log (time, rr_question_id, rr_type_id) VALUES (123, 1, 1);
+INSERT INTO log_rr_answer (log_id, rr_answer_id) VALUES (1, 1);
+INSERT INTO log_rr_answer (log_id, rr_answer_id) VALUES (1, 2);
+*/
 
 // Client implements a client for a SQLite database.
 type Client struct {
@@ -40,6 +64,7 @@ type Client struct {
 // LogEntry represents an entry in the log.
 type LogEntry struct {
 	Question string `db:"question"`
+	Qtype    uint16 `db:"type"`
 	Answer   string `db:"answer"`
 	Time     int64  `db:"time"`
 }
@@ -65,9 +90,15 @@ func New(filename string) (*Client, error) {
 // ReadLog reads the n most recent entries from the log.
 func (c *Client) ReadLog(n int) ([]LogEntry, error) {
 	query := `
-SELECT time, question.value AS question, answer.value AS answer FROM log
-INNER JOIN question ON question.id = log.question_id
-INNER JOIN answer ON answer.id = log.answer_id
+SELECT time,
+       type,
+       rr_question.name AS question,
+       rr_answer.name AS answer
+FROM log
+INNER JOIN rr_question ON rr_question.id = rr_question_id
+INNER JOIN rr_type ON rr_type.id = rr_type_id
+INNER JOIN log_rr_answer ON log_rr_answer.log_id = log.id
+INNER JOIN rr_answer ON rr_answer.id = log_rr_answer.rr_answer_id
 ORDER BY time DESC
 LIMIT $1
 `
@@ -77,7 +108,7 @@ LIMIT $1
 }
 
 // WriteLog writes a new entry to the log.
-func (c *Client) WriteLog(time time.Time, question string, answer string) error {
+func (c *Client) WriteLog(time time.Time, qtype uint16, question string, answers ...string) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	tx, err := c.db.Beginx()
@@ -85,22 +116,43 @@ func (c *Client) WriteLog(time time.Time, question string, answer string) error 
 		return nil
 	}
 	defer rollback(tx)
-	if _, err := tx.Exec("INSERT OR IGNORE INTO question (value) VALUES ($1)", question); err != nil {
+	if _, err := tx.Exec("INSERT OR IGNORE INTO rr_type (type) VALUES ($1)", qtype); err != nil {
 		return err
 	}
-	if _, err := tx.Exec("INSERT OR IGNORE INTO answer (value) VALUES ($1)", answer); err != nil {
+	typeID := 0
+	if err := tx.Get(&typeID, "SELECT id FROM rr_type WHERE type = $1 LIMIT 1", qtype); err != nil {
 		return err
+	}
+	if _, err := tx.Exec("INSERT OR IGNORE INTO rr_question (name) VALUES ($1)", question); err != nil {
+		return err
+	}
+	answerIDs := make([]int, 0, len(answers))
+	for _, answer := range answers {
+		if _, err := tx.Exec("INSERT OR IGNORE INTO rr_answer (name) VALUES ($1)", answer); err != nil {
+			return err
+		}
+		answerID := 0
+		if err := tx.Get(&answerID, "SELECT id FROM rr_answer WHERE name = $1 LIMIT 1", answer); err != nil {
+			return err
+		}
+		answerIDs = append(answerIDs, answerID)
 	}
 	questionID := 0
-	if err := tx.Get(&questionID, "SELECT id FROM question WHERE value = $1 LIMIT 1", question); err != nil {
+	if err := tx.Get(&questionID, "SELECT id FROM rr_question WHERE name = $1 LIMIT 1", question); err != nil {
 		return err
 	}
-	answerID := 0
-	if err := tx.Get(&answerID, "SELECT id FROM answer WHERE value = $1 LIMIT 1", answer); err != nil {
+	res, err := tx.Exec("INSERT INTO log (time, rr_type_id, rr_question_id) VALUES ($1, $2, $3)", time.Unix(), typeID, questionID)
+	if err != nil {
 		return err
 	}
-	if _, err := tx.Exec("INSERT INTO log (question_id, answer_id, time) VALUES($1, $2, $3)", questionID, answerID, time.Unix()); err != nil {
+	logID, err := res.LastInsertId()
+	if err != nil {
 		return err
+	}
+	for _, answerID := range answerIDs {
+		if _, err := tx.Exec("INSERT INTO log_rr_answer (log_id, rr_answer_id) VALUES ($1, $2)", logID, answerID); err != nil {
+			return err
+		}
 	}
 	return tx.Commit()
 }
@@ -114,13 +166,19 @@ func (c *Client) DeleteLogBefore(t time.Time) (err error) {
 		return nil
 	}
 	defer rollback(tx)
-	if _, err := tx.Exec("DELETE FROM log WHERE time < $1", t.Unix()); err != nil {
+	if _, err := tx.Exec("DELETE FROM log_rr_answer WHERE log_id IN (SELECT id FROM log WHERE time < $1)", t.Unix()); err != nil {
 		return err
 	}
-	if _, err := tx.Exec("DELETE FROM question WHERE id NOT IN (SELECT question_id FROM log)", t.Unix()); err != nil {
+	if _, err := tx.Exec("DELETE FROM log WHERE id NOT IN (SELECT log_id FROM log_rr_answer)"); err != nil {
 		return err
 	}
-	if _, err := tx.Exec("DELETE FROM answer WHERE id NOT IN (SELECT answer_id FROM log)", t.Unix()); err != nil {
+	if _, err := tx.Exec("DELETE FROM rr_type WHERE id NOT IN (SELECT rr_type_id FROM log)"); err != nil {
+		return err
+	}
+	if _, err := tx.Exec("DELETE FROM rr_question WHERE id NOT IN (SELECT rr_question_id FROM log)"); err != nil {
+		return err
+	}
+	if _, err := tx.Exec("DELETE FROM rr_answer WHERE id NOT IN (SELECT rr_answer_id FROM log_rr_answer)"); err != nil {
 		return err
 	}
 	return tx.Commit()
--- sql/sql_test.go
+++ sql/sql_test.go
@@ -6,17 +6,28 @@ import (
 	"time"
 )
 
+type rowCount struct {
+	table string
+	rows  int
+}
+
 var tests = []struct {
-	question   string
-	answer     string
-	t          time.Time
-	rows       int
-	logEntries int
+	question  string
+	qtype     uint16
+	answer    string
+	t         time.Time
+	rowCounts []rowCount
 }{
-	{"foo.example.com", "192.0.2.1", time.Date(2019, 6, 15, 22, 15, 10, 0, time.UTC), 1, 1},
-	{"foo.example.com", "192.0.2.1", time.Date(2019, 6, 15, 22, 16, 20, 0, time.UTC), 1, 2},
-	{"bar.example.com", "192.0.2.2", time.Date(2019, 6, 15, 22, 17, 30, 0, time.UTC), 2, 3},
-	{"bar.example.com", "192.0.2.2", time.Date(2019, 6, 15, 22, 18, 40, 0, time.UTC), 2, 4},
+	{"foo.example.com", 1, "192.0.2.1", time.Date(2019, 6, 15, 22, 15, 10, 0, time.UTC),
+		[]rowCount{{"rr_question", 1}, {"rr_answer", 1}, {"log", 1}, {"rr_type", 1}}},
+	{"foo.example.com", 1, "192.0.2.1", time.Date(2019, 6, 15, 22, 16, 20, 0, time.UTC),
+		[]rowCount{{"rr_question", 1}, {"rr_answer", 1}, {"log", 2}, {"rr_type", 1}}},
+	{"bar.example.com", 1, "192.0.2.2", time.Date(2019, 6, 15, 22, 17, 30, 0, time.UTC),
+		[]rowCount{{"rr_question", 2}, {"rr_answer", 2}, {"log", 3}, {"rr_type", 1}}},
+	{"bar.example.com", 1, "192.0.2.2", time.Date(2019, 6, 15, 22, 18, 40, 0, time.UTC),
+		[]rowCount{{"rr_question", 2}, {"rr_answer", 2}, {"log", 4}, {"rr_type", 1}}},
+	{"bar.example.com", 28, "2001:db8::1", time.Date(2019, 6, 15, 23, 4, 40, 0, time.UTC),
+		[]rowCount{{"rr_question", 2}, {"rr_answer", 3}, {"log", 5}, {"rr_type", 2}}},
 }
 
 func testClient() *Client {
@@ -38,18 +49,14 @@ func count(t *testing.T, client *Client, query string, args ...interface{}) int 
 func TestWriteLog(t *testing.T) {
 	c := testClient()
 	for i, tt := range tests {
-		if err := c.WriteLog(tt.t, tt.question, tt.answer); err != nil {
-			t.Errorf("#%d: WriteLog(%s, %q, %q) = %s, want nil", i, tt.t, tt.question, tt.answer, err)
+		if err := c.WriteLog(tt.t, tt.qtype, tt.question, tt.answer); err != nil {
+			t.Errorf("#%d: WriteLog(%q, %d, %q, %q) = %s, want nil", i, tt.t, tt.qtype, tt.question, tt.answer, err)
 		}
-		for _, table := range []string{"question", "answer"} {
-			rows := count(t, c, "SELECT COUNT(*) FROM "+table+" LIMIT 1")
-			if rows != tt.rows {
-				t.Errorf("#%d: got %d rows in %s, want %d", i, rows, table, tt.rows)
+		for _, rowCount := range tt.rowCounts {
+			rows := count(t, c, "SELECT COUNT(*) FROM "+rowCount.table+" LIMIT 1")
+			if rows != rowCount.rows {
+				t.Errorf("#%d: got %d rows in %s, want %d", i, rows, rowCount.table, rowCount.rows)
 			}
-		}
-		logEntries := count(t, c, "SELECT COUNT(*) FROM log LIMIT 1")
-		if logEntries != tt.logEntries {
-			t.Errorf("#%d: got %d log entries, want %d", i, logEntries, tt.logEntries)
 		}
 	}
 }
@@ -57,15 +64,16 @@ func TestWriteLog(t *testing.T) {
 func TestReadLog(t *testing.T) {
 	c := testClient()
 	for i, tt := range tests {
-		if err := c.WriteLog(tt.t, tt.question, tt.answer); err != nil {
-			t.Fatalf("#%d: WriteLog(%s, %q, %q) = %s, want nil", i, tt.t, tt.question, tt.answer, err)
+		if err := c.WriteLog(tt.t, tt.qtype, tt.question, tt.answer); err != nil {
+			t.Fatalf("#%d: WriteLog(%q, %d, %q, %q) = %s, want nil", i, tt.t, tt.qtype, tt.question, tt.answer, err)
 		}
 	}
 	entries := []LogEntry{
-		{Question: "bar.example.com", Answer: "192.0.2.2", Time: 1560637120},
-		{Question: "bar.example.com", Answer: "192.0.2.2", Time: 1560637050},
-		{Question: "foo.example.com", Answer: "192.0.2.1", Time: 1560636980},
-		{Question: "foo.example.com", Answer: "192.0.2.1", Time: 1560636910},
+		{Question: "bar.example.com", Qtype: 28, Answer: "2001:db8::1", Time: 1560639880},
+		{Question: "bar.example.com", Qtype: 1, Answer: "192.0.2.2", Time: 1560637120},
+		{Question: "bar.example.com", Qtype: 1, Answer: "192.0.2.2", Time: 1560637050},
+		{Question: "foo.example.com", Qtype: 1, Answer: "192.0.2.1", Time: 1560636980},
+		{Question: "foo.example.com", Qtype: 1, Answer: "192.0.2.1", Time: 1560636910},
 	}
 	for _, n := range []int{1, len(entries)} {
 		want := entries[:n]
@@ -79,7 +87,7 @@ func TestReadLog(t *testing.T) {
 func TestDeleteLogBefore(t *testing.T) {
 	c := testClient()
 	for i, tt := range tests {
-		if err := c.WriteLog(tt.t, tt.question, tt.answer); err != nil {
+		if err := c.WriteLog(tt.t, tt.qtype, tt.question, tt.answer); err != nil {
 			t.Fatalf("#%d: WriteLog(%s, %q, %q) = %s, want nil", i, tt.t, tt.question, tt.answer, err)
 		}
 	}
@@ -89,8 +97,9 @@ func TestDeleteLogBefore(t *testing.T) {
 	}
 
 	want := []LogEntry{
-		{Question: "bar.example.com", Answer: "192.0.2.2", Time: 1560637120},
-		{Question: "bar.example.com", Answer: "192.0.2.2", Time: 1560637050},
+		{Question: "bar.example.com", Qtype: 28, Answer: "2001:db8::1", Time: 1560639880},
+		{Question: "bar.example.com", Qtype: 1, Answer: "192.0.2.2", Time: 1560637120},
+		{Question: "bar.example.com", Qtype: 1, Answer: "192.0.2.2", Time: 1560637050},
 	}
 	n := 10
 	got, err := c.ReadLog(n)
@@ -99,12 +108,12 @@ func TestDeleteLogBefore(t *testing.T) {
 	}
 
 	question := "foo.example.com"
-	if want, got := 0, count(t, c, "SELECT COUNT(*) FROM question WHERE value = $1", question); got != want {
+	if want, got := 0, count(t, c, "SELECT COUNT(*) FROM rr_question WHERE name = $1", question); got != want {
 		t.Errorf("got %d rows for question %q, want %d", got, question, want)
 	}
 
 	answer := "192.0.2.1"
-	if want, got := 0, count(t, c, "SELECT COUNT(*) FROM answer WHERE value = $1", answer); got != want {
+	if want, got := 0, count(t, c, "SELECT COUNT(*) FROM rr_answer WHERE name = $1", answer); got != want {
 		t.Errorf("got %d rows for answer %q, want %d", got, question, want)
 	}
 }
