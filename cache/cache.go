5ec1cc6ff5c79169783eb70461eabde35a1f1721
Martin Polden <mpolden@mpolden.no>
2020-01-13 22:40:48 +0100

    Defer taking lock until after request completes
    This should be safe now that background tasks are serialized.

--- cache/cache.go
+++ cache/cache.go
@@ -284,8 +284,6 @@ func (c *Cache) Reset() {
 func (c *Cache) prefetch() bool { return c.client != nil }
 
 func (c *Cache) refresh(key uint32, old *dns.Msg) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	q := old.Question[0]
 	msg := dns.Msg{}
 	msg.SetQuestion(q.Name, q.Qtype)
@@ -293,6 +291,8 @@ func (c *Cache) refresh(key uint32, old *dns.Msg) {
 	if err != nil {
 		return // Retry on next request
 	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	if !c.set(key, r) {
 		c.evict(key)
 	}
